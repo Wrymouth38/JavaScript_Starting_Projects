429a8a9ec8ed2174fb62f73f0812812b79513420
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-11 20:58:37 +0100

    change

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 window.alert("Hello, World!");
-var A = "This is a string.";
+var A = "Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.";
 document.write(A);
