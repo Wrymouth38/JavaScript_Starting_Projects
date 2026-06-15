e9ae59ffecc850681f41c881ed065199705fc295
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-15 13:23:59 +0100

    a

--- JavaScript Basic Projects/Project1_expressions_alert/main.js
+++ JavaScript Basic Projects/Project1_expressions_alert/main.js
@@ -1,2 +1,14 @@
+//Window alert
 window.alert("Hello, World!");
+//document.write
 document.write("Hello World one more time");
+
+//Creating two variables
+var sent1 = "This is the beginning of the string";
+var sent2 = " and this is the end of the string";
+
+//displaying variables
+document.write(sent1 + sent2);
+
+//an expression
+document.write(5 + 6);
