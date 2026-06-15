7b9d509c43feb4facaf6085072bef1e31e0bba71
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-15 11:31:30 +0100

    Update ConcatenatingJS.js

--- ConcatenatingJS.js
+++ ConcatenatingJS.js
@@ -1,3 +1,5 @@
-document.write ("\"Be who you are and say what you feel,"
+var B = "\"Be who you are and say what you feel,"
     + " because those who mind don\t matter and those who matter don\t mind.\""
-    + "-Dr. Seuss");
+    + "-Dr. Seuss"
+document.write (B)
+
