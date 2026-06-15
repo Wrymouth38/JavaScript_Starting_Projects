9cf96fe845aaefe7fe8d0c630403b3ac1b96ded2
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-15 13:18:58 +0100

    a

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function my_first_function() {
-    var str = "This is the button text!";
-    document.getElementById("button_text").innerHTML = str;
+function my_first_function() {     //This is my function 
+    var str = "This is the button text!";  //defining variable
+    document.getElementById("button_text").innerHTML = str; //putting the value of the variable into the element
 }
