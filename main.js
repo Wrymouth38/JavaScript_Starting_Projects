01717e0d874e3def1cc1737c355e2bada74a736c
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-15 13:05:48 +0100

    a

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var A = "Hello?", B = "Hellow?", C = "Haloshka";
-document.write(C) ;
-
 function my_first_function() {
     var str = "This is the button text!";
     document.getElementById("button_text").innerHTML = str;
