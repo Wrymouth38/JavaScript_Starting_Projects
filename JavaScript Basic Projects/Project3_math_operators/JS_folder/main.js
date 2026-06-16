52e2f9dccd6077aa32f685d7f0839dca1abb3f9b
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-16 10:25:31 +0100

    sdasda

--- JavaScript Basic Projects/Project3_math_operators/JS_folder/main.js
+++ JavaScript Basic Projects/Project3_math_operators/JS_folder/main.js
@@ -1,9 +1,19 @@
-function addition_Function() {
+function addition_Function() {  //Addition function here
     var addition = 2 + 2;
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
 
-function subtraction_Function() {
+function subtraction_Function() { //This is subtraction function here
     var Subtraction = 5 - 2;
     document.getElementById("Math2").innerHTML = "5 - 2 = " + Subtraction;
+}
+
+function multiplication() { //This is multiplication function here
+    var simple_Math = 6 * 8;
+    document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_Math;
+}
+
+function division() { //This is division function here
+    var simple_Math = 48 / 6;
+    document.getElementById("Math4").innerHTML = "48 / 6 = " + simple_Math;
 }
