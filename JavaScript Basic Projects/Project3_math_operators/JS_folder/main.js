3a840c958de9fa512cdf82e7e191812e6e8b775e
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-16 10:27:54 +0100

    asda

--- JavaScript Basic Projects/Project3_math_operators/JS_folder/main.js
+++ JavaScript Basic Projects/Project3_math_operators/JS_folder/main.js
@@ -17,3 +17,8 @@ function division() { //This is division function here
     var simple_Math = 48 / 6;
     document.getElementById("Math4").innerHTML = "48 / 6 = " + simple_Math;
 }
+
+function alltogether_math() { //This is a more complex math function here
+    var simple_Math = (1 + 2) * 10 / 2 - 5;
+    document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+}
