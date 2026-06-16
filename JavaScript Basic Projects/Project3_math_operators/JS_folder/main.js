95cb44dfc3926392952d902e307d4ee7195e7497
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-16 10:31:12 +0100

    asda

--- JavaScript Basic Projects/Project3_math_operators/JS_folder/main.js
+++ JavaScript Basic Projects/Project3_math_operators/JS_folder/main.js
@@ -22,3 +22,8 @@ function alltogether_math() { //This is a more complex math function here
     var simple_Math = (1 + 2) * 10 / 2 - 5;
     document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
+
+function modulus_Operator() { //This is a modulus operator function here
+    var simple_Math = 25 % 6;
+    document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
