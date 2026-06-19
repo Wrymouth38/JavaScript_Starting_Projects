764d9a3adbc88acf5717ddb0122784a348f26f3c
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-19 21:16:33 +0100

    a

--- JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
+++ JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
@@ -1,3 +1,7 @@
 document.write(typeof "word" + "<br>");
 document.write(typeof 3 + "<br>"); //This will display the type of the number
 document.write("10" + 5 + "<br>");
+
+function my_function() {
+    document.getElementById("test").innerHTML = 0/0; //This will display NaN because you cannot divide by 0
+}
