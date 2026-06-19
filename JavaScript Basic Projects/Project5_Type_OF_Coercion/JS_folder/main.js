d8f7f678df80ddc26f3a8ae8d930db7254b3dc6b
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-19 21:25:21 +0100

    Update main.js

--- JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
+++ JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
@@ -4,8 +4,6 @@ document.write("10" + 5 + "<br>");
 
 function my_function() {
     document.getElementById("test").innerHTML = 0/0; //This will display NaN because you cannot divide by 0
-    document.getElementById("test").innerHTML = isNaN('This is a string') + "<br>"; //This will display true because the string is not a number
-    document.getElementById("test").innerHTML = isNaN('007') + "<br>"; //This will display false because the string is a number
-
-
+    //document.getElementById("test").innerHTML = isNaN('This is a string') + "<br>"; This will display true because the string is not a number
+    //document.getElementById("test").innerHTML = isNaN('007') + "<br>"; This will display false because the string is a number
 }
