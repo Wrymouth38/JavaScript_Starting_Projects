43f122ee57ed5b2e3427f047eded622fde346819
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-19 21:27:32 +0100

    Update main.js

--- JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
+++ JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
@@ -3,7 +3,9 @@ document.write(typeof 3 + "<br>"); //This will display the type of the number
 document.write("10" + 5 + "<br>");
 
 function my_function() {
-    document.getElementById("test").innerHTML = 0/0; //This will display NaN because you cannot divide by 0
+    document.getElementById("test").innerHTML = 0/0 + "<br>"; //This will display NaN because you cannot divide by 0
     //document.getElementById("test").innerHTML = isNaN('This is a string') + "<br>"; This will display true because the string is not a number
     //document.getElementById("test").innerHTML = isNaN('007') + "<br>"; This will display false because the string is a number
 }
+
+document.write(2E310); //This should display infinity
