8a0ce9fa0f34862a42ffdfb797c1f291f5eb2a0e
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-19 21:06:27 +0100

    Update main.js

--- JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
+++ JavaScript Basic Projects/Project5_Type_OF_Coercion/JS_folder/main.js
@@ -1,1 +1,3 @@
-document.write(typeof "word"); //This will display the type of the string
+document.write(typeof "word" + "<br>");
+document.write(typeof 3 + "<br>"); //This will display the type of the number
+document.write("10" + 5 + "<br>");
