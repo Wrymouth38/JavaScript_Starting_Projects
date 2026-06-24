7ea53538c5a7474966149675aacba37e2f835526
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-24 14:11:48 +0100

    a

--- JavaScript Basic Projects/Project7_scope_time_function/JS_folder/main2.js
+++ JavaScript Basic Projects/Project7_scope_time_function/JS_folder/main2.js
@@ -8,3 +8,9 @@ function Add_numbers_2() {
 
 Add_numbers_1();
 Add_numbers_2();
+
+function get_Date() {
+        if (new Date().getHours() < 11) {
+            document.getElementById("Greeting").innerHTML = "How are you today?";
+        }
+    }
