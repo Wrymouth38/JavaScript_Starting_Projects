b4b58b88a29c4785bf3c54c7fc4d3c9f72d489fd
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-26 20:26:43 +0100

    Update main.js

--- JavaScript Basic Projects/Project8_String_methods/JS_folder/main.js
+++ JavaScript Basic Projects/Project8_String_methods/JS_folder/main.js
@@ -2,5 +2,5 @@ function full_sentence() {
     var part_1 = "I am learning";
     var part_2 = " a lot from this course.";
     var whole_sentence = part_1.concat(part_2);
-    document.getElementById("Concatenate").innerHTML = whole_sentence;
+    document.getElementById("concataining").innerHTML = whole_sentence;
 }
