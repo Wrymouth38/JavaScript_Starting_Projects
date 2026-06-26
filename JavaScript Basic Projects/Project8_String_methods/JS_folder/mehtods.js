2b15de97f303cdb69d5bb8429b731fb16b88ec4e
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-06-26 22:22:23 +0100

    a

--- JavaScript Basic Projects/Project8_String_methods/JS_folder/mehtods.js
+++ JavaScript Basic Projects/Project8_String_methods/JS_folder/mehtods.js
@@ -2,3 +2,7 @@ let text = "This is more methods challenge using search";
 let position = text.search("using");
 
 document.getElementById("demo").innerHTML = position;
+
+let text1 = "This is more methods challenge using toUpperCase";
+let upper = text1.toUpperCase();
+document.getElementById("demo1").innerHTML = upper;
