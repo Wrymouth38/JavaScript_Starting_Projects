70696e4b97ff62f7f715a10ce10328d2a644ad1f
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-07-23 14:19:08 +0100

    Update tictactoe.js

--- Tic_Tac_Toe_Assignment/js/tictactoe.js
+++ Tic_Tac_Toe_Assignment/js/tictactoe.js
@@ -14,7 +14,7 @@ function placeXOrO(squareNumber) {
         //this condition checks who's turn it is.
         if (activePlayer === "X") {
             //if active player is equal to "x", the x.png is placed in HTML
-            select.style.backgroundImage = 'url("image/x.png")';
+            select.style.backgroundImage = 'url("images/x.png")';
             //Active player may only be 'X' or 'O' so, if not 'X' it must be 'O'
         } else {
             //If activePlayer is equal to 'O' the o.png is placed in HTML
