37936ff3087d4f06d45485256d6c5a2aac9578f7
Wrymouth38 <108835100+Wrymouth38@users.noreply.github.com>
2026-07-24 12:23:15 +0100

    Update tictactoe.js

--- Tic_Tac_Toe_Assignment/js/tictactoe.js
+++ Tic_Tac_Toe_Assignment/js/tictactoe.js
@@ -122,3 +122,21 @@ function checkWinConditions() {
         if (a === true && b === true && c === true) { return true; }
     }
 }
+
+//This function makes our body element temporarily unclickable.
+function disableClick() {
+    //this makes our body unclickable.
+    body.style.pointerEvents = 'none';
+    //this makes our body clickable again after 1 second.
+    setTimeout(function () { body.style.pointerEvents = 'auto'; }, 1000);
+}
+
+//This function takes a string parameter of the path you set earlier for
+//placement sound('./media/place.mp3)
+function audio(audioURL) {
+    //We create a new audio object and we pass the path as a parameter.
+    let audio = new Audio(audioURL);
+    //play method plays our audio sound
+    audio.play();
+}
+
